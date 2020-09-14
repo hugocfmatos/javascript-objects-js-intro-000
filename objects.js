869c4f4a9c0fd4628ece365aa78b2be2a00b039d
hugo matos <hugocfmatos@gmail.com>
2020-09-14 15:35:40 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,7 +1,8 @@
 var playlist = {nirvana: 'come as you are', crumb:'locket'}
 function updatePlaylist(artistName,songTitle){
-  return Object.assign({},playlist,{[artistName]:songTitle})
+  return Object.assign(playlist,{[artistName]:songTitle})
 }
+
 function removeFromPlaylist(object,key) {
   delete playlist[key]
   return playlist
